777cbef2e1f7e0567e0ac244c50d5f27ca655ac4
Katie Lunder <lunder.katie@gmail.com>
2024-08-11 16:44:38 -0600

    Fixed the Javascript so the search city function works

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@ function refreshWeather(response) {
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
 }
-
 function searchCity(city) {
     let apiKey = "e223321d8eaf8o88a71dtc460e16b347";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(refreshWeather);
-}
-
+  }
+  console.log("Starting the code execution");
+  
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Denver");
