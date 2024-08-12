71138efbc5b6a03d09d5df192d1147b8af1469f6
Katie Lunder <lunder.katie@gmail.com>
2024-08-11 20:00:11 -0600

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,17 +2,44 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windspeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windspeedElement.innerHTML = `${response.data.wind.speed}mph`;
     temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()];
+    
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return    `${day}     ${hours}:${minutes}`;
 }
 function searchCity(city) {
     let apiKey = "e223321d8eaf8o88a71dtc460e16b347";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(refreshWeather);
   }
-  console.log("Starting the code execution");
-  
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
