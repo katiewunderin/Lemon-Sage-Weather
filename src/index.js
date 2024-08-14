46f95fd2aa61a9c89a1a4a54fe325184bfab1c1b
Katie Lunder <lunder.katie@gmail.com>
2024-08-14 12:59:01 -0600

    fixed HTML that was preventing the JS from workign properly.
    Also fixed CSS

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ function refreshWeather(response) {
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windspeedElement.innerHTML = `${response.data.wind.speed}mph`;
+    windspeedElement.innerHTML = `${response.data.wind.speed} mph`;
     temperatureElement.innerHTML = Math.round(temperature);
-    iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class = "weather-app-icon" />`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon" />`;
 
     getForecast(response.data.city);
 }
@@ -42,7 +42,7 @@ function formatDate(date) {
 function searchCity(city) {
     let apiKey = "e223321d8eaf8o88a71dtc460e16b347";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayForecast );
+    axios.get(apiUrl).then(refreshWeather);
   }
 
 function handleSearchSubmit(event) {
@@ -51,32 +51,39 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    return days[date.getDay()];
+}
 function getForecast(city) {
     let apiKey = "e223321d8eaf8o88a71dtc460e16b347";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
     axios(apiUrl).then(displayForecast); 
 }
 function displayForecast(response) {
-    console.log(response.data);
-
     let forecastHTML = "";
-
-    response.data.daily.forEach(function (day) {
-        forecastHTML = 
-            forecastHTML +
-            `
-                <div class="forecast">
-                    <div class="forecast-day">
-                        <div class="forecast-date">${day}</div>
-                        <div class="forecast-icon"> ⛅️ </div>
-                        <div class="forecast-temp-day">H - <strong> </strong><small>℉</small></div>
-                        <div class="forecast-temp-night">L - <strong> </strong><small>℉</small></div>
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHTML += `
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+                <div class="weather-forecast-temperature">
+                    <div class="weather-forecast-temperature-day">
+                        <strong>${Math.round(day.temperature.maximum)}º</strong>
+                    </div>
+                    <div class="weather-forecast-temperature-night">
+                        ${Math.round(day.temperature.minimum)}º
                     </div>
                 </div>
-            `;
+            </div>
+                    `;
+        }
     });
 
-let forecastElement = document.querySelector("#forecast");
+let forecastElement = document.querySelector("#weather-forecast");
 forecastElement.innerHTML = forecastHTML;
 }
 
